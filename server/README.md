3a76ca052966abe7d15707c84560cff5b51e5d21
Tyler Moore <tylermoore124@gmail.com>
2019-10-16 22:52:33 -0400

    Update to use mongoose services

--- server/README.md
+++ server/README.md
@@ -1,4 +1,4 @@
-# server
+# anime-db
 
 > 
 
@@ -14,7 +14,7 @@ Getting up and running is as easy as 1, 2, 3.
 2. Install your dependencies
 
     ```
-    cd path/to/server
+    cd path/to/anime-db
     npm install
     ```
 
--- server/src/services/users/users.class.js
+++ server/src/services/users/users.class.js
@@ -1,11 +1,5 @@
-const { Service } = require('feathers-mongodb');
+const { Service } = require('feathers-mongoose');
 
 exports.Users = class Users extends Service {
-  constructor(options, app) {
-    super(options);
-    
-    app.get('mongoClient').then(db => {
-      this.Model = db.collection('users');
-    });
-  }
+  
 };
