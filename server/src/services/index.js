e91a3512df15103c7f203eb816ff9d9e6c8a9fa6
Tyler Moore <tylermoore124@gmail.com>
2019-12-03 09:46:35 -0500

    setting up mailchimp api

--- server/src/services/index.js
+++ server/src/services/index.js
@@ -1,7 +1,9 @@
 const users = require('./users/users.service.js');
 const anime = require('./anime/anime.service.js');
+const mailingList = require('./mailing-list/mailing-list.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(users);
   app.configure(anime);
+  app.configure(mailingList);
 };
---
+++ server/src/services/mailing-list/mailing-list.class.js
@@ -0,0 +1,45 @@
+/* eslint-disable no-unused-vars */
+const request = require("request-promise");
+const Mailchimp = require("mailchimp-api-v3");
+
+const mailchimp = new Mailchimp("apikey");
+
+exports.MailingList = class MailingList {
+  constructor(options) {
+    this.options = options || {};
+  }
+
+  async find(params) {
+    return [];
+  }
+
+  async get(id, params) {
+    return {
+      id,
+      text: `A new message with ID: ${id}!`
+    };
+  }
+
+  async create(data, params) {
+    mailchimp
+      .post("/automations/5113917/emails/5113921/queue", data)
+      .then(function(results) {
+        res.send(results);
+      })
+      .catch(function(err) {
+        res.send(err);
+      });
+  }
+
+  async update(id, data, params) {
+    return data;
+  }
+
+  async patch(id, data, params) {
+    return data;
+  }
+
+  async remove(id, params) {
+    return { id };
+  }
+};
